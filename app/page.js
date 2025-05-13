cae95dfa50f499d47b0ad7f6661d712464048e9b
Hannah <155262308+hannahsoftdev@users.noreply.github.com>
2025-05-13 13:00:27 -0600

    Assignment Submission
    Completed assignment

--- app/page.js
+++ app/page.js
@@ -1,10 +1,17 @@
+import Link from "next/link";
 
 
 export default function Home() {
   return (
     <main>
-      <h1></h1>
-      <p></p>
+      <h1 className="text-3xl">CPRG 306: Web Development 2 - Assignments</h1>
+        <ul>
+        <li>
+          <Link href="./week-2/"
+          className= "hover:text-green-500 hover:underline"
+          >Week 2 Assignment</Link>
+        </li>
+      </ul>
     </main>
 
   );
---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+        <h1>Hannah Mae Tolentino</h1>
+        <Link href="https://github.com/hannahsoftdev/cprg306-assignments">https://github.com</Link>
+    </div>
+  );
+}
