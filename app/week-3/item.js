72bd4e3c28c9af9069871800202bf468874e5ae1
Hannah Mae Tolentino <hannahmae.tolentino@edu.sait.ca>
2025-06-02 12:59:16 -0600

    Finalized assignment

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,8 +1,9 @@
 export default function Item({ name, quantity, category }) {
-  return ( 
-    <li>
-      <p>{name}</p>
-      <p>Buy {quantity} in {category}</p>
+  return (
+    <li className="bg-blue-950 p-2 mb-4">
+      <p className="text-white font-semibold">{name}</p>
+      <p className="text-white">Buy {quantity} in {category}</p>
     </li>
   );
 }
+
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,16 +1,10 @@
-import ItemList from "./item-list";
-
+import ItemList from './item-list';
 
 export default function Page() {
   return (
-    <main className="bg-black text-white">
-        <div className="text-3xl font-bold mb-4">
-            <h1>Shopping List</h1>
-        </div>
-
-        <div className="text-1xl bg-gray-800 px-2 py-1 rounded">
-            <ItemList />
-        </div>
+    <main className="p-5 bg-black">
+      <h1 className="text-3xl text-white font-bold mb-2">Shopping List</h1>
+      <ItemList />
     </main>
   );
 }
